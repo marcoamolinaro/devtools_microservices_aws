a291ddfd2d754615ec65ee81123a53861940f42d
--global <--global>
2023-01-09 20:21:08 -0300

    Remove flight services

--- flightservices/src/main/java/com/scm/springcloud/repositories/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scm.springcloud.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.scm.springcloud.models.Flight;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-
-}
